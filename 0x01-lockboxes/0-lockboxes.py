bfc8e26565fcdf734454e44407e4d0454d94d15b
malakehab2003 <lokaehab2003@gmail.com>
2024-05-03 01:22:02 +0300

    create lockboxes task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,12 +3,16 @@
 
 
 def canUnlockAll(boxes):
-    def dfs(box_index):
-        visited.add(box_index)
-        for key in boxes[box_index]:
-            if key not in visited:
-                dfs(key)
-
+    """ Create canUnlockAll to check if all boxes unlocked """
+    mySet = {0}
     visited = set()
-    dfs(0)
+
+    while mySet:
+        box = mySet.pop()
+        visited.add(box)
+
+        for key in boxes[box]:
+            if key not in visited:
+                mySet.add(key)
+
     return len(visited) == len(boxes)
