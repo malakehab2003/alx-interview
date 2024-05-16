91618dd791e5e277b38a2f792cf57b8c47fed305
malakehab2003 <lokaehab2003@gmail.com>
2024-05-16 17:11:21 +0300

    finish log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,58 +2,42 @@
 """ solve the log parsing task """
 import sys
 
-
 def parse_line(line):
-    """ parse the line """
-    parse = line.split()
-    if len(parse) == 9:
+    """Parse a line of input and extract file size and status code."""
+    parts = line.split()
+    if len(parts) == 9 and parts[4] == '"GET':
         try:
-            size = int(parse[-1])
-        except Exception:
+            file_size = int(parts[-1])
+            status_code = int(parts[-2])
+            return file_size, status_code
+        except ValueError:
             pass
-        try:
-            status = int(parse[-2])
-        except Exception:
-            pass
-        return size, status
     return None, None
 
+def print_status(total_size):
+    """Print total file size and status codes."""
+    print('File size:', total_size)
+    for key in sorted(status_dict.keys()):
+        value = status_dict[key]
+        if value > 0:
+            print('{}: {}'.format(key, value))
+            status_dict[key] = 0
 
 total_size = 0
 count = 0
-status_dict = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
+status_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-
-def print_status(total_size):
-    print('File size: {}'.format(total_size))
-    for key, value in status_dict.items():
-        if value == 0:
-            continue
-        print('{}: {}'.format(key, value))
-        status_dict[key] = 0
-
-
-for line in sys.stdin:
-    try:
+try:
+    for line in sys.stdin:
         size, status = parse_line(line)
-        count += 1
-        if total_size is not None and status is not None:
+        if size is not None and status is not None:
             total_size += size
             status_dict[status] += 1
-
+        count += 1
         if count == 10:
             print_status(total_size)
-            count = 0
             total_size = 0
-    except KeyboardInterrupt:
-        print_status(total_size)
-        sys.exit(0)
+            count = 0
+except KeyboardInterrupt:
+    print_status(total_size)
+    sys.exit(0)
