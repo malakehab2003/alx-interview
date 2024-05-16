1bcdd1bd6d721080f73dc41f3bbfcc58612a52f0
malakehab2003 <lokaehab2003@gmail.com>
2024-05-16 17:30:13 +0300

    finish log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,41 +2,55 @@
 """ solve the log parsing task """
 import sys
 
+
 def parse_line(line):
-    """Parse a line of input and extract file size and status code."""
-    parts = line.split()
-    if len(parts) == 9 and parts[4] == '"GET':
+    """ parse the line """
+    parse = line.split()
+    if len(parse) == 9:
         try:
-            file_size = int(parts[-1])
-            status_code = int(parts[-2])
-            return file_size, status_code
-        except ValueError:
+            size = int(parse[-1])
+        except Exception:
             pass
+        try:
+            status = int(parse[-2])
+        except Exception:
+            pass
+        return size, status
     return None, None
 
-def print_status(total_size):
-    """Print total file size and status codes."""
-    print('File size:', total_size)
-    for key in sorted(status_dict.keys()):
-        value = status_dict[key]
-        if value > 0:
-            print('{}: {}'.format(key, value))
 
 total_size = 0
 count = 0
-status_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_dict = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0
+}
+
+
+def print_status(total_size):
+    print('File size: {}'.format(total_size))
+    for key, value in status_dict.items():
+        if value == 0:
+            continue
+        print('{}: {}'.format(key, value))
 
 try:
     for line in sys.stdin:
         size, status = parse_line(line)
-        if size is not None and status is not None:
+        count += 1
+        if total_size is not None and status is not None:
             total_size += size
             status_dict[status] += 1
-        count += 1
+
         if count == 10:
             print_status(total_size)
             count = 0
-    print_status(total_size)
 except KeyboardInterrupt:
     print_status(total_size)
     raise
