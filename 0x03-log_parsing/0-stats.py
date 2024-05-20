925cfa52595a21ae2dd0314866b68842478746c3
malakehab2003 <lokaehab2003@gmail.com>
2024-05-20 20:41:45 +0300

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -19,38 +19,39 @@ def parse_line(line):
     return None, None
 
 
-total_size = 0
-count = 0
-status_dict = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
+if __name__ == '__main__':
+    total_size = 0
+    count = 0
+    status_dict = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
 
 
-def print_status(total_size):
-    print('File size: {:d}'.format(total_size))
-    for key in sorted(status_dict.keys()):
-        value = status_dict[key]
-        if value == 0:
-            continue
-        print('{:s}: {:d}'.format(key, value))
+    def print_status(total_size):
+        print('File size: {:d}'.format(total_size))
+        for key in sorted(status_dict.keys()):
+            value = status_dict[key]
+            if value == 0:
+                continue
+            print('{:s}: {:d}'.format(key, value))
 
-try:
-    for line in sys.stdin:
-        size, status = parse_line(line)
-        count += 1
-        if total_size is not None and status is not None:
-            total_size += size
-            status_dict[status] += 1
+    try:
+        for line in sys.stdin:
+            size, status = parse_line(line)
+            count += 1
+            if total_size is not None and status is not None:
+                total_size += size
+                status_dict[status] += 1
 
-        if count == 10:
-            print_status(total_size)
-            count = 0
-except (KeyboardInterrupt, EOFError):
-    print_status(total_size)
+            if count == 10:
+                print_status(total_size)
+                count = 0
+    except (KeyboardInterrupt, EOFError):
+        print_status(total_size)
