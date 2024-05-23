ef055ed620eb762e55fffed47e4086d7202c2724
malakehab2003 <lokaehab2003@gmail.com>
2024-05-23 23:00:13 +0300

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,36 +3,30 @@
 
 
 def validUTF8(data):
-    """Check if valid UTF-8"""
-    remaining_bytes = 0
+    """ check if valid UTF8 """
+    flag = 0
     for i in data:
-        if i > 255:
+        if i > 255 or i < 0:
             return False
 
-        binary_number = bin(i)[2:].rjust(8, '0')
+        binary_number = bin(i).replace('0b', '').rjust(8, '0')[-8:]
 
-        # For the first byte
-        if remaining_bytes == 0:
-            # Count the number of leading 1s to determine the length of the UTF-8 character
-            for bit in binary_number:
-                if bit == '0':
-                    break
-                remaining_bytes += 1
+        count_1 = 0
 
-            # If this is a 1-byte character (no leading 1s)
-            if remaining_bytes == 0:
+        for num in binary_number:
+            if num != '1':
+                break
+            count_1 += 1
+
+        if flag > 0:
+            flag -= 1
+            if count_1 != 1:
+                return False
+        else:
+            if count_1 == 0:
                 continue
-
-            # If remaining_bytes is 1 or greater than 4, it's an invalid character
-            if remaining_bytes == 1 or remaining_bytes > 4:
+            elif count_1 == 1 or count_1 > 4:
                 return False
-
-        else:  # For continuation bytes
-            # Check that the byte is a valid continuation byte (starts with 10)
-            if not (binary_number.startswith('10') and remaining_bytes > 0):
-                return False
-
-            remaining_bytes -= 1
-
-    # All characters should be fully parsed, and no remaining bytes should be expected
-    return remaining_bytes == 0
+            else:
+                flag = count_1 - 1
+    return flag == 0
