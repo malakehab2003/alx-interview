891dc8a414ccd0377cf7df97a6f70bf2105fe12a
malakehab2003 <lokaehab2003@gmail.com>
2024-05-23 23:13:03 +0300

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,35 +1,13 @@
-#!/usr/bin/python3
-""" Create validUTF8 function """
-
-
 def validUTF8(data):
-    """ check if valid UTF8 """
+    """Check if valid UTF8."""
     flag = 0
     for i in data:
-        if i > 255 or i < 0:
+        if i >= 0x80 and flag == 0:
             return False
-
-        if type(i) is not int:
-            return False
-
-        binary_number = bin(i).replace('0b', '').rjust(8, '0')[-8:]
-
-        count_1 = 0
-
-        for num in binary_number:
-            if num != '1':
-                break
-            count_1 += 1
-
-        if flag > 0:
-            flag -= 1
-            if count_1 != 1:
+        if i & 0xC0 == 0x80:
+            flag += 1
+            if flag > 1 or (flag == 1 and i & 0xE0 == 0xA0):
                 return False
         else:
-            if count_1 == 0:
-                continue
-            elif count_1 == 1 or count_1 > 4:
-                return False
-            else:
-                flag = count_1 - 1
+            flag = 0
     return flag == 0
