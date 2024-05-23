97a126e98736abdd54a9899d05e7a9882bd10808
malakehab2003 <lokaehab2003@gmail.com>
2024-05-23 22:45:07 +0300

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,20 +6,28 @@ def validUTF8(data):
     """ check if valid UTF8 """
     flag = 0
     for i in data:
+        if i > 255:
+            return False
+        
         binary_number = bin(i).replace('0b', '').rjust(8, '0')[-8:]
+
         count_1 = 0
+
         for num in binary_number:
             if num != '1':
                 break
             count_1 += 1
+
+
         if flag > 0:
             flag -= 1
             if count_1 != 1:
                 return False
-        if count_1 == 0:
-            continue
-        elif count_1 == 1 or count_1 > 4:
-            return False
         else:
-            flag = count_1 - 1
-    return True      
+            if count_1 == 0:
+                continue
+            elif count_1 == 1 or count_1 > 4:
+                return False
+            else:
+                flag = count_1 - 1
+    return flag == 0      
