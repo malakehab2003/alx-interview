c186b006c56b5452b7db9ce7d6c8b16c62ca9e03
root <lokaehab2003@gmail.com>
2024-06-02 17:11:03 -0700

    final try

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,96 +1,73 @@
 #!/usr/bin/python3
-""" solve n queen problem """
+"""
+Solve N-Queens problem
+"""
+
 import sys
 
-
-# check the number of args
+# Check the number of args
 if len(sys.argv) != 2:
     print('Usage: nqueens N')
     sys.exit(1)
 
-# check that the second arg is a number
+# Check that the second arg is a number
 try:
     n = int(sys.argv[1])
-except Exception:
+except ValueError:
     print("N must be a number")
     sys.exit(1)
 
-# check if number smaller than 4
+# Check if number is less than 4
 if n < 4:
     print('N must be at least 4')
     sys.exit(1)
 
-# check col and positive and negative diagonal
-cols = []
-pos_diagonal = []
-neg_diagonal = []
+def is_safe(board, row, col):
+    """ Check if it's safe to place a queen at board[row][col] """
+    
+    # Check this row on left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
 
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
 
-# search for all possible solutions
-my_list = []
-i = 0
-col = 0
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
 
+    return True
 
-def remove_old_data(i):
-    """
-    remove data of the queen which aready moved
-    """
-    remove_item = []
+def solve_nqueens_util(board, col):
+    """ Util function to solve N-Queens problem """
+    if col >= n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        print(solution)
+        return True
 
-    # get the item to remove
-    for sublist in my_list:
-        if sublist and sublist[0] == i:
-            remove_item.append(sublist)
+    res = False
+    for i in range(n):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1) or res
+            board[i][col] = 0  # backtrack
 
-    # remove item from my_list
-    for item in remove_item:
-        my_list.remove(item)
+    return res
 
-    # remove item from neg_diagonal, pos_diagonal and cols
-    row, col = remove_item[0][:2]
-    pos_diagonal.remove(row - col)
-    neg_diagonal.remove(row + col)
-    cols.remove(col)
-    return col
+def solve_nqueens():
+    """ Solve N-Queens problem """
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    if not solve_nqueens_util(board, 0):
+        print("Solution does not exist")
+        return False
+    return True
 
-
-while i < n:
-    while col in range(n):
-        # if the place don't have any problem
-        # with positive and negative diagonal and columns place it
-        if (col not in cols and
-                (i - col) not in pos_diagonal and
-                (i + col) not in neg_diagonal):
-            cols.append(col)
-            pos_diagonal.append(i - col)
-            neg_diagonal.append(i + col)
-            my_list.append([i, col])
-            if i == n - 1:
-                break
-            i += 1
-            col = 0
-        elif col == n - 1:
-            # if we reached to the end of the row and can't add the queen
-            # we return to the last added queen and move it to the next place
-            # and modify cols, neg_diagonal and pos_diagonal
-            i -= 1
-            col = remove_old_data(i)
-            if col < n - 1:
-                col += 1
-            else:
-                i -= 1
-                col = remove_old_data(i) + 1
-
-        else:
-            col += 1
-    if len(my_list) == n:
-        print(my_list)
-        i = 0
-        col = cols[0] + 1
-        if col == n - 1:
-            break
-        cols.clear()
-        my_list.clear()
-        pos_diagonal.clear()
-        neg_diagonal.clear()
+solve_nqueens()
