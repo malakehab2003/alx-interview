9c471a13ccf57f55f0512207e9961230788a2d31
malakehab2003 <lokaehab2003@gmail.com>
2024-06-06 03:48:49 +0300

    task 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/* get the actors name */
+
+const request = require('request');
+
+const args = process.argv;
+
+if (args.length !== 3) {
+    process.exit(1)
+};
+
+id = args[2]
+url = 'https://swapi-api.alx-tools.com/api/films/' + id
+
+request(url, (err, res, body) => {
+        if (err) {
+            console.log(err)
+        } else {
+            const chars = JSON.parse(body).characters;
+            getChars(chars, 0)
+        };
+    }
+); 
+
+function getChars(chars, index) {
+    request(chars[index], (err, res, body) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(JSON.parse(body).name);
+            if (index + 1 < chars.length) {
+                getChars(chars, index + 1);
+            };
+        };
+    });
+};
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,1 @@
+solve star war api request
