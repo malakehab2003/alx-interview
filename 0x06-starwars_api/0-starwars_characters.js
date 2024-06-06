2789ccbe47640b60ec9e01aaa7aa939cb291fac5
malakehab2003 <lokaehab2003@gmail.com>
2024-06-06 03:57:17 +0300

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,25 @@
 #!/usr/bin/node
-/* get the actors name */
+/* Get the actors name */
 
 const request = require('request');
 
 const args = process.argv;
 
 if (args.length !== 3) {
-    process.exit(1)
-};
+    process.exit(1);
+}
 
-id = args[2]
-url = 'https://swapi-api.alx-tools.com/api/films/' + id
+const id = args[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
 request(url, (err, res, body) => {
-        if (err) {
-            console.log(err)
-        } else {
-            const chars = JSON.parse(body).characters;
-            getChars(chars, 0)
-        };
+    if (err) {
+        console.log(err);
+    } else {
+        const chars = JSON.parse(body).characters;
+        getChars(chars, 0);
     }
-); 
+});
 
 function getChars(chars, index) {
     request(chars[index], (err, res, body) => {
@@ -30,7 +29,7 @@ function getChars(chars, index) {
             console.log(JSON.parse(body).name);
             if (index + 1 < chars.length) {
                 getChars(chars, index + 1);
-            };
-        };
+            }
+        }
     });
-};
+}
