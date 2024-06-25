db88efb348bc0876c6c9ee2484ca30441027d1b5
malakehab2003 <lokaehab2003@gmail.com>
2024-06-25 20:29:25 +0300

    task 0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,13 +5,21 @@
 def check_surrounded(grid, i, j):
     """ check the island surrounding is water or not """
     perimeter = 0
-    if grid[i - 1][j] == 0:
+    if i == 0:
         perimeter += 1
-    if grid[i + 1][j] == 0:
+    elif grid[i - 1][j] == 0:
         perimeter += 1
-    if grid[i][j - 1] == 0:
+    if i == len(grid) - 1:
         perimeter += 1
-    if grid[i][j + 1] == 0:
+    elif grid[i + 1][j] == 0:
+        perimeter += 1
+    if j == 0:
+        perimeter += 1
+    elif grid[i][j - 1] == 0:
+        perimeter += 1
+    if j == len(grid[i]) - 1:
+        perimeter += 1
+    elif grid[i][j + 1] == 0:
         perimeter += 1
 
     return perimeter
