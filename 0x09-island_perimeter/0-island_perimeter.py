3cfd96e28521241415afb7cf8aff12f948fe5fea
malakehab2003 <lokaehab2003@gmail.com>
2024-06-25 20:16:24 +0300

    task 0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -20,8 +20,8 @@ def check_surrounded(grid, i, j):
 def island_perimeter(grid):
     """ calculate perimeter of island as grid """
     perimeter = 0
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[i]) - 1):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 0:
                 continue
             else:
