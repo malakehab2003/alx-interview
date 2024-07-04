1973706bfe83d652c3540f8ab00ce9f0fda65ef4
malakehab2003 <lokaehab2003@gmail.com>
2024-07-04 19:34:32 +0300

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,8 +9,9 @@ def is_prime(n):
     for i in range(2, n):
         if n % i == 0:
             return False
-        
+
     return True
+
 
 def isWinner(x, nums):
     """ solve prime game problem """
