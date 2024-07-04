2ac9e4c89e3ebadedc3b4f04563efc98ac3811f4
malakehab2003 <lokaehab2003@gmail.com>
2024-07-04 20:20:45 +0300

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,11 +4,17 @@
 
 def is_prime(n):
     """ check if n is prime """
-    if n < 2:
+    if n <= 1:
         return False
-    for i in range(2, n):
-        if n % i == 0:
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
+        i += 6
     return True
 
 
