f435fd098fed7d7b22d7c83ffa2e7e5779dae9db
malakehab2003 <lokaehab2003@gmail.com>
2024-07-07 01:17:25 +0300

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,43 +2,49 @@
 """ implement isWinner function """
 
 
-def is_prime(n):
-    """ check if n is prime """
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+def sieve_of_eratosthenes(max_n):
+    """ get only prime numbers """
+    is_prime = [True] * (max_n + 1)
+    p = 2
+    while p * p <= max_n:
+        if is_prime[p]:
+            for i in range(p * p, max_n + 1, p):
+                is_prime[i] = False
+        p += 1
+    primes = [p for p in range(2, max_n + 1) if is_prime[p]]
+    return primes
 
 
 def isWinner(x, nums):
-    """ solve prime game problem """
-    n = 0
-    ben = 0
-    maria = 0
-    for i in range(x):
-        if nums[i] == 0:
-            continue
-        for j in range(nums[i] + 1):
-            if is_prime(j):
-                n += 1
-        if j == 1:
-            ben += 1
-        elif n % 2 == 0:
-            ben += 1
+    """ get the winner of maria and ben """
+    if x == 0 or not nums:
+        return None
+
+    max_n = max(nums)
+    primes = sieve_of_eratosthenes(max_n)
+
+    def count_primes_up_to(n):
+        """ return count of prime numbers less than n """
+        count = 0
+        for p in primes:
+            if p > n:
+                break
+            count += 1
+        return count
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        primes_count = count_primes_up_to(n)
+        if primes_count % 2 == 1:
+            maria_wins += 1
         else:
-            maria += 1
-        n = 0
-    if ben > maria:
-        return "Ben"
-    elif maria > ben:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
         return None
---
+++ 0x0A-primegame/main_test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+isWinner = __import__('test').isWinner
+
+
+print("Winner: {}".format(isWinner(3, [0, 0, 0])))
